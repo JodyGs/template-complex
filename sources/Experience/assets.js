8721c80de32d2a38d805fac534fae5789543c0cb
JodyGs <demajn@tuta.io>
2023-03-06 22:44:21 +0100

    first commit

---
+++ sources/Experience/assets.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'loTexture', source: '/assets/lo.png', type: 'texture' },
+        ]
+    }
+]
